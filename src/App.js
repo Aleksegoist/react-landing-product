8dd3fb723010a97ec86fe1052fc252a6c14a4b13
Aleksandr Yehorov <64478695+Aleksegoist@users.noreply.github.com>
2022-08-18 01:20:31 +0300

    add

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from './components/header/Header';
 import Hero from './components/hero/Hero';
+import Product from './components/product/Product';
 import './index.css';
 
 function App() {
   return <div className='App'>
     <Header />
-    {/* <Hero /> */}
+    <Hero />
+    <Product />
   </div>;
 }
 
