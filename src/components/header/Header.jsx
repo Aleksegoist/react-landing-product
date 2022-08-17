600a4d183d039a3e308c08b4239f07c8c6401662
Aleksandr Yehorov <64478695+Aleksegoist@users.noreply.github.com>
2022-08-17 10:44:12 +0300

    add header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,31 +2,39 @@ import React from 'react';
 import style from './Header.module.css';
 import LogoLogo from '../../assets/logo_logo.png';
 import LogoText from '../../assets/logo_product.png';
+import Theme from '../../assets/theme.png';
 
 const Header = () => {
   return (
     <div className={style.header}>
       <div className='container'>
         <div className={style.header_box}>
-          <div className={style.header_logo}>
-            <img src={LogoLogo} alt='/' />
-            <img src={LogoText} alt='/' />
+          <div className={style.header_box2}>
+            <div className={style.header_logo}>
+              <img src={LogoLogo} alt='/' />
+              <img src={LogoText} alt='/' />
+            </div>
+            <div className={style.header_menu}>
+              <ul>
+                <li>
+                  <a href='/'>Product</a>
+                </li>
+                <li>
+                  <a href='/'>Customers</a>
+                </li>
+                <li>
+                  <a href='/'>Pricing</a>
+                </li>
+                <li>
+                  <a href='/'>Resources</a>
+                </li>
+              </ul>
+            </div>
           </div>
-          <div className={style.header_menu}>
-            <ul className={style.menu_items}>
-              <li>
-                <a href='/'>Product</a>
-              </li>
-              <li>
-                <a href='/'>Customers</a>
-              </li>
-              <li>
-                <a href='/'>Pricing</a>
-              </li>
-              <li>
-                <a href='/'>Resources</a>
-              </li>
-            </ul>
+          <div className={style.header_btn}>
+            <button>Sign In</button>
+            <button>Sign Up</button>
+            <img src={Theme} alt='/' />
           </div>
         </div>
       </div>
